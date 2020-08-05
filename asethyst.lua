8004a2ecf747e34ad1bf5125c919e0d4830f0630
Maxi Nothnagel <15906207+exxaggerating@users.noreply.github.com>
2020-08-05 16:30:56 +0200

    Replace single with double quotes

--- asethyst.lua
+++ asethyst.lua
@@ -2,7 +2,7 @@ local dlg
 local sprite
 
 local config = {
-   version = 'v0.2.0',
+   version = "v0.2.0",
    api_version = 11,
 }
 
@@ -12,14 +12,14 @@ function export_as_grid()
    local x_count = dlg.data.cols
    local y_count = math.ceil(#sprite.frames / dlg.data.cols)
 
-   table.insert(data, 'Grid((')
-   table.insert(data, '\ttexture_width: ' .. sprite.width * x_count .. ',')
-   table.insert(data, '\ttexture_height: ' .. sprite.height * y_count .. ',')
-   table.insert(data, '\tcolumns: ' .. dlg.data.cols .. ',')
-   table.insert(data, '\tsprite_count: ' .. #sprite.frames)
-   table.insert(data, '))\n')
+   table.insert(data, "Grid((")
+   table.insert(data, "\ttexture_width: " .. sprite.width * x_count .. ",")
+   table.insert(data, "\ttexture_height: " .. sprite.height * y_count .. ",")
+   table.insert(data, "\tcolumns: " .. dlg.data.cols .. ",")
+   table.insert(data, "\tsprite_count: " .. #sprite.frames)
+   table.insert(data, "))\n")
 
-   return table.concat(data, '\n')
+   return table.concat(data, "\n")
 end
 
 function export_as_list()
@@ -28,91 +28,91 @@ function export_as_list()
    local x_count = dlg.data.cols
    local y_count = math.ceil(#sprite.frames / dlg.data.cols)
 
-   table.insert(data, 'List((')
-   table.insert(data, '\ttexture_width: ' .. sprite.width * x_count .. ',')
-   table.insert(data, '\ttexture_height: ' .. sprite.height * y_count .. ',')
-   table.insert(data, '\tsprites: [')
+   table.insert(data, "List((")
+   table.insert(data, "\ttexture_width: " .. sprite.width * x_count .. ",")
+   table.insert(data, "\ttexture_height: " .. sprite.height * y_count .. ",")
+   table.insert(data, "\tsprites: [")
 
    for i = 0, #sprite.frames - 1 do
       local x = i % dlg.data.cols
       local y = math.floor(i / dlg.data.cols)
 
-      local s = '\t\t(\n'
-         .. '\t\t\tx: ' .. x * sprite.width .. ',\n'
-         .. '\t\t\ty: ' .. y * sprite.height .. ',\n'
-         .. '\t\t\twidth: ' .. sprite.width .. ',\n'
-         .. '\t\t\theight: ' .. sprite.height .. '\n'
-         .. '\t\t),'
+      local s = "\t\t(\n"
+         .. "\t\t\tx: " .. x * sprite.width .. ",\n"
+         .. "\t\t\ty: " .. y * sprite.height .. ",\n"
+         .. "\t\t\twidth: " .. sprite.width .. ",\n"
+         .. "\t\t\theight: " .. sprite.height .. "\n"
+         .. "\t\t),"
       
       table.insert(data, s)
    end
 
-   table.insert(data, '\t],')
-   table.insert(data, '))')
+   table.insert(data, "\t],")
+   table.insert(data, "))")
 
-   return table.concat(data, '\n')
+   return table.concat(data, "\n")
 end
 
 function build_dialog()
    local dlg = Dialog{
-      title = 'Asethyst Exporter :) - ' .. config.version,
+      title = "Asethyst Exporter :) - " .. config.version,
    }
 
    dlg:file{
-      id = 'sheet_data',
-      label = 'Spritesheet Data:',
-      title = 'Save Spritesheet Data',
+      id = "sheet_data",
+      label = "Spritesheet Data:",
+      title = "Save Spritesheet Data",
       save = true,
-      filetypes = { 'ron' },
+      filetypes = { "ron" },
       entry = true
    }
 
    dlg:combobox{
-      id = 'ron_type',
-      label = 'Sprite Type:',
-      option = 'List',
-      options = { 'Grid', 'List' }
+      id = "ron_type",
+      label = "Sprite Type:",
+      option = "List",
+      options = { "Grid", "List" }
    }
 
    dlg:number{
-      id = 'cols',
-      label = 'Column Count',
+      id = "cols",
+      label = "Column Count",
       decimals = 0
    }
 
    dlg:separator()
 
    dlg:button{
-      id = 'save',
-      text = 'Save'
+      id = "save",
+      text = "Save"
    }
 
-   dlg:button{text = 'Cancel'}
+   dlg:button{text = "Cancel"}
 
    return dlg
 end
 
 function build_error_dialog(msg, expected_value, found_value)
    local dlg = Dialog{
-      title = 'Asethyst Exporter :( - ' .. config.version,
+      title = "Asethyst Exporter :( - " .. config.version,
    }
 
    dlg:separator{
-      text = 'ERROR'
+      text = "ERROR"
    }
 
    dlg:label{
-      label = 'Message:',
+      label = "Message:",
       text = msg
    }
 
    dlg:label{
-      label = 'Expected Value:',
+      label = "Expected Value:",
       text = expected_value
    }
 
    dlg:label{
-      label = 'Found Value:',
+      label = "Found Value:",
       text = found_value
    }
 
@@ -122,7 +122,7 @@ end
 function init()
    if app.apiVersion < config.api_version then
       local dlg = build_error_dialog(
-         'Your Aseprite API version is out of date!',
+         "Your Aseprite API version is out of date!",
          config.api_version,
          app.apiVersion
       )
@@ -142,11 +142,11 @@ function init()
       sprite = app.activeSprite
       local filename = dlg.data.sheet_data
 
-      local data = dlg.data.ron_type == 'Grid' 
+      local data = dlg.data.ron_type == "Grid" 
          and export_as_grid() 
          or export_as_list()
 
-      local file = io.open(filename, 'w')
+      local file = io.open(filename, "w")
       file:write(data)
    end
 end
